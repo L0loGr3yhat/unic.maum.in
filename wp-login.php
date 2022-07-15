95e7774f1fd84f2c48d4b7b152c414445cf5e860
LoloGreyhat <109199712+L0loGr3yhat@users.noreply.github.com>
2022-07-15 17:25:30 +0800

    Create wp-login.php

---
+++ wp-login.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+    <title>LOLoGreyhat</title>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+    <meta property="og:title" content="LoloGreyhat">
+    <meta name="description" content="Ups,your security getdown">
+    <meta property="og:description" content="All defacer">
+    <meta property="og:image" content="https://i.pinimg.com/564x/d6/36/bb/d636bbe4b344d86ea15f22c64b9d64b7.jpg">
+    <meta name="theme-color" content="#fff">
+    <style type='text/css'>body,a:hover{cursor:url(../cur.cursors-4u.net/symbols/sym-1/sym46.cur),progress!important;}</style>
+</head>
+<body bgcolor="black" text="white" oncontextmenu="return false;" onkeydown="return false;" onmousedown="return false;" onclick="play()">
+<style type="text/css">
+    center {
+        font-family: Courier;
+    }
+    img {
+        opacity: 80%;
+    }
+    red {
+        color: red;
+    }
+</style>
+<script language="JavaScript">
+    window.onbeforeunload = confirmExit;
+    function confirmExit() {
+        return "are you sure ? wkwk";
+    }
+</script>
+<table width="100%" height="80%">
+    <td>    <script>alert("LoloGreyhat Was Here")
+        var nama = prompt("Siapa nama kamu?", "");
+    </script>
+        <center><font colot="white" size="2">copyright © 2022 lolo</font>
+            <img src="https://d.top4top.io/p_2368gwpp40.png" width="220" height="220">
+            <br>Take Over By<red><i>Lolo</i></red><br><font size="2">hello admin,your security website is low<br>please tighten the security of your website<br><br>
+            <marquee behavior="alternate" scrollamount="5" style="border:1px solid;" width="80%"><font color="orange" face="courier">
+             Greetz: SUH4IRYIL_31 - LurzyWasHere ~ ShadowcrewTeam - NullCrew cyber army - 
+                 sangrimau - Noname24"</font></marquee>
+                 <br><br><font color="red"size="2">[Play|Musiv]</font><br>
+   <audio autoplay="autoplay" controls="controls" width="55px" height="20px"> <source src="https://l.top4top.io/m_2368ucnsz0.mp4"> </audio>
+
+</script>
+<br>
+</div>
+<!-- END SOUND CODE V3.15 -->
+
+<center
+</center>
+</body>
+</html>
